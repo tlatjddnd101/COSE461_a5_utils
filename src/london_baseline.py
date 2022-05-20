7b230e9c12c33eb435e1207ee5bdf755cbd05647
tlatjddnd101 <54761455+tlatjddnd101@users.noreply.github.com>
2022-05-20 17:17:31 +0900

    question 2.4

--- src/london_baseline.py
+++ src/london_baseline.py
@@ -1,3 +1,3 @@
-# Calculate the accuracy of a baseline that simply predicts "London" for every example in the dev set.
+# Calculate the accuracy of a baseline that simply predicts 'London' for every example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
