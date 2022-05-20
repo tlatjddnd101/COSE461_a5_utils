7fc48da41d14e724c55d4e26128a92fb511883b4
tlatjddnd101 <54761455+tlatjddnd101@users.noreply.github.com>
2022-05-20 17:18:49 +0900

    Update

--- src/model.py
+++ src/model.py
@@ -107,7 +107,7 @@ class GPT(nn.Module):
         # if we are given some desired targets also calculate the loss
         loss = None
         if targets is not None:
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=0)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index = 0)
 
         return logits, loss
 
